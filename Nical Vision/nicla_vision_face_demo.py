46ead43ee5f53feea24ff4bee8747bfc620ce0b1
limengdu <37475446+limengdu@users.noreply.github.com>
2024-01-02 13:43:16 +0800

    Update

---
+++ Nical Vision/nicla_vision_face_demo.py
@@ -0,0 +1,32 @@
+import pyb # Import module for board related functions
+import sensor # Import the module for sensor related functions
+import image # Import module containing machine vision algorithms
+import time # Import module for tracking elapsed time
+
+sensor.reset() # Resets the sensor
+sensor.set_pixformat(sensor.RGB565) # Sets the sensor to RGB
+sensor.set_framesize(sensor.QVGA) # Sets the resolution to 320x240 px
+sensor.set_vflip(True) # Flips the image vertically
+sensor.set_hmirror(True) # Mirrors the image horizontally
+sensor.skip_frames(time = 2000) # Skip some frames to let the image stabilize
+
+# Define the min/max LAB values we're looking for
+thresholdsFace = (29, 71, 4, 127, 3, 127)
+
+clock = time.clock() # Instantiates a clock object
+
+while(True):
+    clock.tick() # Advances the clock
+    img = sensor.snapshot() # Takes a snapshot and saves it in memory
+    # Find blobs with a minimal area of 50x50 = 2500 px
+    # Overlapping blobs will be merged
+    blobs = img.find_blobs([thresholdsFace], area_threshold=2500, merge=True)
+
+    # Draw blobs
+    for blob in blobs:
+        # Draw a rectangle where the blob was found
+        img.draw_rectangle(blob.rect(), color=(0,255,0))
+        # Draw a cross in the middle of the blob
+        img.draw_cross(blob.cx(), blob.cy(), color=(0,255,0))
+        pyb.delay(50) # Pauses the execution for 50ms
+        print(clock.fps()) # Prints the framerate to the serial console
